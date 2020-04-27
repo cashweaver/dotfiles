7cec31f039e544400247298465a587d93c82834b
cashweaver <cashbweaver@gmail.com>
2020-04-26 21:08:48 -0700

    Extrace expected programs into a separate file

--- check_programs.sh
+++ check_programs.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 # Check for expected programs. The user is expected to manually install missing programs themselves.
 
-programs=(
-    "nvim"
-    "pipenv"
-    )
 missing_programs=()
 found_programs=()
 
-for program in "${programs[@]}"; do
-    if [[ ! -x $(command -v "${program}") ]]; then
-        missing_programs+=("${program}")
-    else
-        found_programs+=("${program}")
-    fi
-done
+while read program; do
+  if [[ ! -x $(command -v "${program}") ]]; then
+    missing_programs+=("${program}")
+  else
+    found_programs+=("${program}")
+  fi
+done <expected_programs.txt
 
 if [[ "${#missing_programs[@]}" != "0" ]]; then
-    echo "Found ${#found_programs[@]}/${#programs[@]} programs."
-    echo ""
-    echo "Missing programs:"
-    for missing_program in "${missing_programs[@]}"; do
-        echo "${missing_program}"
-    done
+  echo "Found ${#found_programs[@]}/$(wc -l < expected_programs.txt) programs."
+  echo ""
+  echo "Missing programs:"
+  for missing_program in "${missing_programs[@]}"; do
+    echo "${missing_program}"
+  done
 else
-    echo "Found all programs."
+  echo "Found all programs."
 fi
