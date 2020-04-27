34ae05e839946b88f10092ea793d01a6c75ba496
cashweaver <cashbweaver@gmail.com>
2020-04-26 21:09:57 -0700

    Add brightness scripts

---
+++ scripts/config/monitors.sh
@@ -0,0 +1,9 @@
+# Helpful monitor variables
+
+north_monitor_output_name="DP-4"
+south_monitor_output_name="DP-0"
+west_monitor_output_name="DP-2"
+
+north_monitor_crtc_number=1
+south_monitor_crtc_number=0
+west_monitor_crtc_number=2
