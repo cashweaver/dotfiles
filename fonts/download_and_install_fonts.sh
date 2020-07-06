36dbacbbb5d6ed3f5c9650a2a95bdcf4fc90cf02
Cash Weaver <cashweaver@google.com>
2020-07-06 10:17:11 -0700

    Only get fonts once; get latest font

--- fonts/download_and_install_fonts.sh
+++ fonts/download_and_install_fonts.sh
@@ -1,31 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # Download and install fonts
-# Assume the system is Ubuntu
 
-FONTS_PATH="~/.fonts"
-DOWNLOADS_PATH="~/Downloads"
+FONTS_PATH="$HOME/.fonts"
+DOWNLOADS_PATH="$HOME/Downloads"
 
-if [[ ! -d $FONTS_PATH ]]; then
+# ref: https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+latest_version() {
+  curl -sI https://github.com/$1/releases/latest |
+    grep -Po 'tag\/\K(v\S+)' |
+    sed 's/^v//'
+}
+
+if [[ ! -d "$FONTS_PATH" ]]; then
   mkdir ~/.fonts
 fi
 
-if [[ ! -d $DOWNLOADS_PATH ]]; then
+if [[ ! -d "$DOWNLOADS_PATH" ]]; then
   mkdir ~/Downloads
 fi
 
 echo -e "\nAll fonts are downloaded to $DOWNLOADS_PATH."
 
-
-echo -e "\n\nIosevka"
 if [[ ! -d "${FONTS_PATH}/iosevka" ]]; then
   echo "Downloading Iosevka..."
-  iosevka_target="iosevka-term-ss09-3.0.0-rc.8"
+  version="$(latest_version "be5invis/iosevka")"
+  iosevka_target="ttf-iosevka-term-ss09-${version}"
   cd ~/Downloads
-  wget "https://github.com/be5invis/Iosevka/releases/download/v3.0.0-rc.8/${iosevka_target}.zip"
+  wget "https://github.com/be5invis/Iosevka/releases/download/v${version}/${iosevka_target}.zip"
   echo "Installing..."
   mkdir -p ~/.fonts/iosevka
   unzip "${iosevka_target}.zip" -d ${iosevka_target} > /dev/null
-  mv "${iosevka_target}/ttf/"* ~/.fonts/iosevka
+  mv "${iosevka_target}/ttf/"* "${FONTS_PATH}/iosevka"
 else
   echo "Iosevka has already been installed."
 fi
