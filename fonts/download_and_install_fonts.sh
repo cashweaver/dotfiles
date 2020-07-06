cc6617280bb41124e36ce38c6750ecdad489afd1
Cash Weaver <cashweaver@google.com>
2020-07-06 09:40:18 -0700

    Don't get already got fonts

--- fonts/download_and_install_fonts.sh
+++ fonts/download_and_install_fonts.sh
@@ -2,23 +2,30 @@
 # Download and install fonts
 # Assume the system is Ubuntu
 
-if [[ ! -d ~/.fonts ]]; then
+FONTS_PATH="~/.fonts"
+DOWNLOADS_PATH="~/Downloads"
+
+if [[ ! -d $FONTS_PATH ]]; then
   mkdir ~/.fonts
 fi
 
-if [[ ! -d ~/Downloads ]]; then
+if [[ ! -d $DOWNLOADS_PATH ]]; then
   mkdir ~/Downloads
 fi
 
-echo -e "\nAll fonts are downloaded to ~/Downloads."
+echo -e "\nAll fonts are downloaded to $DOWNLOADS_PATH."
 
 
 echo -e "\n\nIosevka"
-echo "Downloading..."
-iosevka_target="iosevka-term-ss09-3.0.0-rc.8"
-cd ~/Downloads
-wget "https://github.com/be5invis/Iosevka/releases/download/v3.0.0-rc.8/${iosevka_target}.zip"
-echo "Installing..."
-mkdir -p ~/.fonts/iosevka
-unzip "${iosevka_target}.zip" -d ${iosevka_target} > /dev/null
-mv "${iosevka_target}/ttf/"* ~/.fonts/iosevka
+if [[ ! -d "${FONTS_PATH}/iosevka" ]]; then
+  echo "Downloading Iosevka..."
+  iosevka_target="iosevka-term-ss09-3.0.0-rc.8"
+  cd ~/Downloads
+  wget "https://github.com/be5invis/Iosevka/releases/download/v3.0.0-rc.8/${iosevka_target}.zip"
+  echo "Installing..."
+  mkdir -p ~/.fonts/iosevka
+  unzip "${iosevka_target}.zip" -d ${iosevka_target} > /dev/null
+  mv "${iosevka_target}/ttf/"* ~/.fonts/iosevka
+else
+  echo "Iosevka has already been installed."
+fi
